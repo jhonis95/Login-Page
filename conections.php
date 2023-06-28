ff3473a9116c31d6b1a3b321d011e9625ba321fa
jonatan de oliveira <j.jhonis95@gmail.com>
2023-06-27 23:21:34 -0300

    restating the app

--- conections.php
+++ conections.php
@@ -3,5 +3,7 @@ $conn =new mysqli('localhost','jonatan','0000','logging_users');
     if($conn -> connect_errno){
         echo "Failed to connect to MySQL: ";
         exit();
+    }else{
+        echo "connected with data base";
     }
 ?>
--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const inputUser= document.getElementById('user');
 const inputPassword= document.getElementById('password');
 const errorMessenge= document.getElementById("errorMessenge");
 
-submitBTN.addEventListener("click",(evt)=>{
+submitBTN.addEventListener("submit",(evt)=>{
         evt.preventDefault();
+        console.log(inputUser.value);
         console.log(inputPassword.value);
         if(!inputUser.value||!inputPassword.value){
             errorMessenge.innerText="User or Password is missing"
             errorMessenge.style.visibility='visible';
         }else {
-            fetch("login.php")
+            fetch("/login.php")
         }
 })
 
