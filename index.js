389196dd2e823a3653fcb259703ae19c3c261ee5
jonatan de oliveira <j.jhonis95@gmail.com>
2023-06-27 23:55:39 -0300

    testing the fetch API

--- index.js
+++ index.js
@@ -3,15 +3,33 @@ const inputUser= document.getElementById('user');
 const inputPassword= document.getElementById('password');
 const errorMessenge= document.getElementById("errorMessenge");
 
-submitBTN.addEventListener("submit",(evt)=>{
-        evt.preventDefault();
-        console.log(inputUser.value);
-        console.log(inputPassword.value);
+submitBTN.addEventListener("click",(evt)=>{
+        // evt.preventDefault();
+        if(evt){
+            console.log(inputUser.value);
+            console.log(inputPassword.value);
+        }
         if(!inputUser.value||!inputPassword.value){
             errorMessenge.innerText="User or Password is missing"
             errorMessenge.style.visibility='visible';
         }else {
-            fetch("/login.php")
+            let data={user:inputUser.value, password:inputPassword.value}
+            let myHeaders = new Headers();
+            let myInit = { method: 'POST',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default',
+               body: JSON.stringify(data) };
+
+            fetch("/login.php",myInit).then((res)=>{
+                console.log()
+                if(res.status=="200"){
+                    
+                }
+                // if(res){
+                //     window.location.replace("AppUser.php")
+                // }
+            })
         }
 })
 
